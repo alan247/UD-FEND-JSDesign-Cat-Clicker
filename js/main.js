6c9f60b5d39a6da38165b616bfab03f69792935c
Alan Sánchez Pérez Peña <alan247@gmail.com>
2015-12-16 18:41:06 -0600

    Add model. Add list render function. Add styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,94 @@
 
+var model = {
+    "data": [
+      {
+        "name":  "Patotas",
+        "image": "/images/patotas.jpg",
+        "clickCount": 0
+      },
+      {
+        "name":  "Roo",
+        "image": "/images/roo.jpg",
+        "clickCount": 0
+      },
+      {
+        "name":  "Caty",
+        "image": "/images/caty.jpg",
+        "clickCount": 0
+      },
+      {
+        "name":  "McMau",
+        "image": "/images/mcmau.jpg",
+        "clickCount": 0
+      },
+      {
+        "name":  "Panterota",
+        "image": "/images/panterota.jpg",
+        "clickCount": 0
+      }
+    ],
+
+    getOneCat: function(catName){
+      var allCats = this.data;
+
+      for(var i = 0; i < allCats.length; i++) {
+        if(allCats[i].name === catName) {
+          return allCats[i];
+        }
+     }
+  }
+};
+
+
+var manager = {
+	getCatInfo: function(catName){
+    return model.getOneCat(catName);
+	},
+
+  getAllCatsInfo: function() {
+    return model.data;
+  }
+};
+
+var viewList = {
+	init: function() {
+
+
+
+	},
+
+	render: function() {
+    var htmlStr = '';
+    var allCats = manager.getAllCatsInfo();
+
+    for(var i = 0; i < allCats.length; i++){
+      htmlStr += '<li class="cat-item">'+
+              allCats[i].name +
+              '</li>';
+    }
+    $('#cat-list').append(htmlStr);
+    return htmlStr;
+  }
+
+};
+
+var viewCat = {};
+
+
+
+
+
+
+
+
+
+
+
+/*
 var cats =  {
-	'names' : [
-		'Allison',
-		'Johnson'
+	"names" : [
+		"Allison",
+		"Johnson"
 	]
 }
 
@@ -14,7 +100,7 @@ var Cat = function(){
 	for(var i = 0; i < catNames.length; i ++) {
 		console.log(catNames[i]);
 		// var
-		$('#cats-container').append('<div class="cat ' + catNames[i] + '"><div class="counter"><span>0</span></div><img onclick="startCats.addClick()" class="cat-picture" src="images/' + catNames[i] + '.jpg" />' + catNames[i] + '</div>');
+		$("#cats-container").append("<div class="cat " + catNames[i] + ""><div class="counter"><span>0</span></div><img onclick="startCats.addClick()" class="cat-picture" src="images/" + catNames[i] + ".jpg" />" + catNames[i] + "</div>");
 	}
 };
 
@@ -22,16 +108,18 @@ Cat.prototype.addClick = function() {
 
 	console.log(this);
 
-	// $('.cat-picture').on('click', function() {
-	// 	var currentCount = Number($('#counter').html());
+	// $(".cat-picture").on("click", function() {
+	// 	var currentCount = Number($("#counter").html());
 	// 	currentCount ++;
 
-	// 	$('#counter').html(currentCount);
+	// 	$("#counter").html(currentCount);
 	// });
 };
 
 
 var startCats = new Cat();
 
-// $('.cat-picture').on('click', startCats.addClick());
+// $(".cat-picture").on("click", startCats.addClick());
+
+*/
 
